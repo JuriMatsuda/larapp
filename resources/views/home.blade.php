6de4abdc0810bbe93fd922b8a8cf514f884fb44d
Juri Matsuda <matsuda.juri@gmail.com>
2018-11-11 18:57:37 +0900

    add LoginController@logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,10 @@ Hello, World!!
 
 @if(Auth::check())
     {{ \Auth::user()->name }}さん
+    <a href="/auth/logout">ログアウト</a>
 @else
     ゲストさん<br>
+    <a href="/auth/login">ログイン</a>
     <a href="/auth/register">会員登録</a>
 @endif
 </body>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('/home', function () {
 Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('auth/register', 'Auth\RegisterController@register');
 
-// ユーザー認証
+// ログイン
 Route::get('auth/login', 'Auth\LoginController@showLoginFrom');
 Route::post('auth/login', 'Auth\LoginController@login');
+
+// ログアウト
+Route::get('auth/logout', 'Auth\LoginController@logout');
