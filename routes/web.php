f2f3ace0aec077ea3264440d671210296e3fd024
Juri Matsuda <matsuda.juri@gmail.com>
2018-11-11 18:41:04 +0900

    add routing for login

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function() {
+Route::get('/home', function () {
     return view('home');
 });
 
+// ユーザー登録
 Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('auth/register', 'Auth\RegisterController@register');
+
+// ユーザー認証
+Route::get('auth/login', 'Auth\LoginController@showLoginFrom');
+Route::post('auth/login', 'Auth\LoginController@login');
