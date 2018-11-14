5aa054ecae0c783a685d933bfee6e0066fa75b22
Juri Matsuda <matsuda.juri@gmail.com>
2018-11-14 21:41:47 +0900

    set MailHog

--- app/Listeners/RegisteredListener.php
+++ app/Listeners/RegisteredListener.php
@@ -5,17 +5,26 @@ namespace App\Listeners;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+use Illuminate\Mail\Mailer;
 
 class RegisteredListener
 {
+    /**
+     * @var Mailer
+     */
+    private $mailer;
+    private $eloquent;
+
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Mailer $mailer, User $eloquent)
     {
-        //
+        $this->mailer = $mailer;
+        $this->eloquent = $eloquent;
     }
 
     /**
@@ -26,6 +35,9 @@ class RegisteredListener
      */
     public function handle(Registered $event)
     {
-        //
+        $user = $this->eloquent->findOrFail($event->user->getAuthIdentifier());
+        $this->mailer->raw('会員登録が完了しました', function ($message) use ($user) {
+            $message->subject('会員登録メール')->to($user->email);
+        });
     }
 }
